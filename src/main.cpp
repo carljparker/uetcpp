2063cf19b4223ef8fb7b45eb907acc1855113f7c
Kevin Dalleau <kdalleau@rosalind4.loria.fr>
2019-09-05 13:47:25 +0200

    Libraries cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,13 @@
 #include <fstream>
 #include <sstream>
 #include <iterator>
-#include <nlohmann/json.hpp>
 #include <cstdio>
-#include <iomanip>
 #include <Dense>
 #include <chrono>
 #include <omp.h>
 #include <fstream>
 #include <unordered_set>
 
-using json = nlohmann::json;
 using Eigen::MatrixXd;
 using Eigen::MatrixXd;
 using namespace std;
@@ -28,64 +25,7 @@ using std::vector;
 using std::chrono::duration;
 using std::chrono::duration_cast;
 using std::chrono::high_resolution_clock;
-
-double mean(const std::vector<float> &v)
-{
-    double sum = 0;
-
-    for (auto &each : v)
-        sum += each;
-
-    return sum / v.size();
-}
-
-double sd(const std::vector<float> &v)
-{
-    double square_sum_of_difference = 0;
-    double mean_var = mean(v);
-    auto len = v.size();
-
-    double tmp;
-    for (auto &each : v)
-    {
-        tmp = each - mean_var;
-        square_sum_of_difference += tmp * tmp;
-    }
-
-    return std::sqrt(square_sum_of_difference / (len - 1));
-}
-double random_split(vector<float> values, int type)
-{
-
-    double split;
-    auto result = std::minmax_element(std::begin(values), std::end(values));
-    double sum = std::accumulate(values.begin(), values.end(), 0.0);
-    double mean = sum / values.size();
-
-    double sq_sum = std::inner_product(values.begin(), values.end(), values.begin(), 0.0);
-    double stdev = std::sqrt(sq_sum / values.size() - mean * mean);
-    srand(time(NULL));
-    std::random_device rd;
-    std::default_random_engine generator(rd());
-    if (type == 0)
-    {
-        std::normal_distribution<double> distribution(mean, stdev);
-
-        // cout << mean << " " << stdev << endl;
-        do
-        {
-            split = distribution(generator);
-        } while (split < *result.first || split > *result.second);
-
-        // std::uniform_real_distribution<double> distribution(*result.first, *result.second);
-    }
-    else
-    {
-        std::uniform_int_distribution<int> dist(0, values.size() - 1);
-        split = values[dist(generator)];
-    }
-    return split;
-}
+using std::sort;
 
 void print(std::vector<float> const &input)
 {
@@ -101,6 +41,39 @@ void print(std::vector<int> const &input)
     {
         std::cout << input[i] << ' ';
     }
+}
+
+float random_split(vector<float> values, int type)
+{
+
+    float split;
+    auto result = std::minmax_element(std::begin(values), std::end(values));
+
+    srand(time(NULL));
+    std::random_device rd;
+    std::mt19937_64 generator(rd());
+    if (type == 0)
+    {
+        // std::normal_distribution<double> distribution(mean, stdev);
+
+        // // cout << mean << " " << stdev << endl;
+        // do
+        // {
+        //     split = distribution(generator);
+        // } while (split < *result.first || split > *result.second);
+
+        std::uniform_real_distribution<float> distribution(*result.first, *result.second);
+        split = distribution(generator);
+        // split = *result.second + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(*result.first-*result.second))); //distribution(generator);
+    }
+    else
+    {   
+        std::uniform_int_distribution<int> dist(0, values.size() - 1);
+        split = values[dist(generator)];
+    }
+
+    // cout << split << endl;
+    return split;
 }
 
 vector<float> getColumn(const vector<vector<float>> &v, int attribute)
@@ -123,25 +96,13 @@ vector<vector<float>> getRows(const vector<vector<float>> &v, vector<int> rows)
     return (out);
 }
 
-vector<float> remove(std::vector<float> v)
-{
-    std::vector<float>::iterator itr = v.begin();
-    std::unordered_set<int> s;
-
-    for (auto curr = v.begin(); curr != v.end(); ++curr)
-    {
-        if (s.insert(*curr).second)
-            *itr++ = *curr;
-    }
-
-    v.erase(itr, v.end());
-    return (v);
-}
-
 int performSplit(const vector<vector<float>> &dataset, vector<int> &attributesIndices,
-                 vector<int> attributes, const vector<int> &coltypes, vector<int> &left, vector<int> &right)
+                 const vector<int> attributes, const vector<int> &coltypes, vector<int> &left, vector<int> &right)
 {
-    int randIndex = rand() % attributesIndices.size();
+    std::random_device rd;
+    std::mt19937_64 mt(rd());
+    std::uniform_int_distribution<int> dist (0,attributesIndices.size()-1);
+    int randIndex = dist(mt);
     int attributeIndex = attributesIndices[randIndex];
     *attributesIndices.erase(attributesIndices.begin() + randIndex);
     int attribute = attributes[attributeIndex];
@@ -151,19 +112,19 @@ int performSplit(const vector<vector<float>> &dataset, vector<int> &attributesIn
 
     data = getColumn(dataset, attribute);
 
-    std::unordered_set<double> set;
+    std::unordered_set<float> set;
 
-    for (const double &i : data)
+    for (const float &i : data)
     {
         set.insert(i);
     }
 
-    if (set.size() == 1)
+    if (set.size() <= 1)
     {
         return 1;
     }
 
-    double value = random_split(data, type);
+    float value = random_split(data, type);
 
     for (int i = 0; i < data.size(); i++)
     {
@@ -183,7 +144,10 @@ int performSplit(const vector<vector<float>> &dataset, vector<int> &attributesIn
 int performSplit(const vector<vector<float>> &dataset, vector<int> &attributesIndices,
                  const vector<int> attributes, const vector<int> &coltypes, vector<int> &left, vector<int> &right, const vector<int> &nodeIndices)
 {
-    int randIndex = rand() % attributesIndices.size();
+    std::random_device rd;
+    std::mt19937_64 mt(rd());
+    std::uniform_int_distribution<int> dist (0,attributesIndices.size()-1);
+    int randIndex = dist(mt);
     int attributeIndex = attributesIndices[randIndex];
     *attributesIndices.erase(attributesIndices.begin() + randIndex);
     int attribute = attributes[attributeIndex];
@@ -196,17 +160,17 @@ int performSplit(const vector<vector<float>> &dataset, vector<int> &attributesIn
 
     data = getColumn(rows, attribute);
 
-    std::unordered_set<double> set;
-    for (const double &i : data)
+    std::unordered_set<float> set;
+    for (const float &i : data)
     {
         set.insert(i);
     }
 
-    if (set.size() == 1)
+    if (set.size() <= 1)
     {
         return 1;
     }
-    double value = random_split(data, type);
+    float value = random_split(data, type);
 
     for (int i = 0; i < data.size(); i++)
     {
@@ -229,46 +193,24 @@ struct Node
 };
 
 MatrixXd build_randomized_tree_and_get_sim(const vector<vector<float>> &data,
-                                                        const double &nmin, const vector<int> &coltypes, int nTrees)
+                                                        const int &nmin, const vector<int> &coltypes, int nTrees)
 {
-
     int nrows = data.size();
     int ncols = data.front().size();
     MatrixXd matrix = MatrixXd::Zero(nrows, nrows);
     #pragma omp parallel for
     for (int loop = 0; loop < nTrees; loop++)
     {
-        srand(time(NULL));
-
         queue<Node> nodes;
 
-        vector<int> instancesList;
-        for (int i = 0; i < nrows; i++)
-        {
-            instancesList.push_back(i);
-        }
         vector<int> attributes, attributes_indices;
         for (int i = 0; i < ncols; i++)
         {
             attributes_indices.push_back(i);
             attributes.push_back(i);
         }
-
         vector<int> left_indices, right_indices;
         performSplit(data, attributes_indices, attributes, coltypes, left_indices, right_indices);
-
-        vector<int> left_instances, right_instances;
-        vector<vector<float>> left_data, right_data;
-
-        for (int j : left_indices)
-        {
-            left_data.push_back(data[j]);
-        }
-
-        for (int j : right_indices)
-        {
-            right_data.push_back(data[j]);
-        }
 
         if (left_indices.size() < nmin)
         {
@@ -306,15 +248,14 @@ MatrixXd build_randomized_tree_and_get_sim(const vector<vector<float>> &data,
             nodes.push(currentNode);
         }
 
-        if (left_indices.size() + right_indices.size() != 150)
-        {
-            cout << "Error" << endl;
-        }
+        // if (left_indices.size() + right_indices.size() != 150)
+        // {
+        //     cout << "Error" << endl;
+        // }
 
         // Root node successfully has two children. Now, we iterate over these children.
         while (!nodes.empty())
         {
-
             if (attributes_indices.size() < 1)
             {
                 while (!nodes.empty())
@@ -330,7 +271,6 @@ MatrixXd build_randomized_tree_and_get_sim(const vector<vector<float>> &data,
                         }
                     }
                 }
-
                 break;
             }
 
@@ -338,85 +278,59 @@ MatrixXd build_randomized_tree_and_get_sim(const vector<vector<float>> &data,
             nodes.pop();
 
             vector<int> nodeIndices = currentNode.indices;
-            if (nodeIndices.size() >= 150)
-            {
-                cout << "Error with nodeIndices size" << endl;
-            }
+
             vector<int> left_indices, right_indices;
-            if (nodeIndices.size() >= nmin)
+            int colNum = performSplit(data, attributes_indices, attributes, coltypes, left_indices, right_indices, nodeIndices);
+            if (colNum == 1) // We have a column with only one unique value
+            { 
+                for (int instance1 : nodeIndices)
+                {
+                    for (int instance2 : nodeIndices)
+                    {
+                        #pragma omp atomic
+                        matrix(instance1, instance2) += 1.0/nTrees;
+                    }
+                }
+                continue;
+            }
+
+            if (left_indices.size() < nmin)
             {
-                if (performSplit(data, attributes_indices, attributes, coltypes, left_indices, right_indices, nodeIndices) == 1) // We have a column with only one unique value
-                { 
-                    for (int instance1 : nodeIndices)
+                for (int instance1 : left_indices)
+                {
+                    for (int instance2 : left_indices)
                     {
-                        for (int instance2 : nodeIndices)
-                        {
-                            #pragma omp atomic
-                            matrix(instance1, instance2) += 1.0/nTrees;
-                        }
+                        #pragma omp atomic
+                        matrix(instance1, instance2) += 1.0/nTrees;
                     }
-                    cout << "Lower bound number of values reached" << endl;
-                    continue;
-                }
-
-                vector<int> left_instances, right_instances;
-                // vector<vector<float>> left_data, right_data;
-
-                for (int j : left_indices)
-                {
-                    left_data.push_back(data[j]);
-                }
-
-                for (int j : right_indices)
-                {
-                    right_data.push_back(data[j]);
-                }
-
-                if (left_instances.size() + right_instances.size() >= 150)
-                {
-                    cout << "Error" << endl;
-                }
-                if (left_indices.size() < nmin)
-                {
-                    for (int instance1 : left_indices)
-                    {
-                        for (int instance2 : left_indices)
-                        {
-                            #pragma omp atomic
-                            matrix(instance1, instance2) += 1.0/nTrees;
-                        }
-                    }
-                }
-
-                else
-                {
-                    Node currentNode = {left_indices};
-                    nodes.push(currentNode);
-                }
-
-                if (right_indices.size() < nmin)
-                {
-                    for (int instance1 : right_indices)
-                    {
-                        for (int instance2 : right_indices)
-                        {
-                            #pragma omp atomic
-                            matrix(instance1, instance2) += 1.0/nTrees;
-                        }
-                    }
-                }
-
-                else
-                {
-                    Node currentNode = {right_indices};
-                    nodes.push(currentNode);
                 }
             }
+
             else
             {
-                cout << "Else"
-                     << "\n";
+                Node currentNode = {left_indices};
+                nodes.push(currentNode);
             }
+
+            if (right_indices.size() < nmin)
+            {
+                for (int instance1 : right_indices)
+                {
+                    for (int instance2 : right_indices)
+                    {
+                        #pragma omp atomic
+                        matrix(instance1, instance2) += 1.0/nTrees;
+                    }
+                }
+            }
+
+            else
+            {
+                Node currentNode = {right_indices};
+                nodes.push(currentNode);
+            }
+        
+
         }
     }
     return matrix;
@@ -449,9 +363,9 @@ int main()
 {
 
     vector<vector<float>> data;
-    int nTrees = 10000;
+    int nTrees = 250;
 
-    vector<vector<float>> j = readCSV("./data/iris_wl.csv", ',');
+    vector<vector<float>> j = readCSV("./data/iris.csv", ',');
     vector<int> labels;
     int nrows = j.size();
 
